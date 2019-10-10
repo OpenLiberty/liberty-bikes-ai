17084fec0028b2ae29c379d6fb3c7b64d809e0be
orcook <orcook@us.ibm.com>
2019-10-10 08:07:53 -0500

    Remove MP Config class / remove MP Rest Client class

--- src/main/java/org/libertybikes/ai/restclient/GameServiceClient.java
+++ src/main/java/org/libertybikes/ai/restclient/GameServiceClient.java
@@ -16,13 +16,4 @@ import org.libertybikes.ai.service.AIConfiguration;
  * but can be overridden with externalized configuration via MP Config to point to a
  * remote game-service instance.
  */
-@Path("/")
-@Produces(MediaType.APPLICATION_JSON)
-@RegisterRestClient(baseUri = "http://localhost:8080", configKey = AIConfiguration.GAME_SERVICE_KEY)
-public interface GameServiceClient {
-
-    @GET
-    @Path("/party/describe")
-    public Map<String, Object> describe();
-
-}
+public interface GameServiceClient {}
