740ed4644c053e830b727ec81c5eb7b8c4f3e083
orcook <orcook@us.ibm.com>
2019-10-10 09:12:18 -0500

    Remove StartupProcedure web listener

--- src/main/java/org/libertybikes/ai/service/StartupProcedure.java
+++ src/main/java/org/libertybikes/ai/service/StartupProcedure.java
@@ -9,15 +9,4 @@ import javax.servlet.annotation.WebListener;
  * This class will be initialized when the app first starts. We can use this hook
  * to automatically register our AI service with the core Liberty Bikes services
  */
-@WebListener
-public class StartupProcedure implements ServletContextListener {
-
-    @Inject
-    RegistrationBean registration;
-
-    @PostConstruct
-    public void joinFirstRound() {
-        registration.joinRound();
-    }
-
-}
+public class StartupProcedure {}
