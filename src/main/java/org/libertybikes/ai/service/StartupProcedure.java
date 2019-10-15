994cbf34be5a3522ae9cc266903822ebce5d7a53
orcook <orcook@us.ibm.com>
2019-10-15 15:17:15 -0500

    master resync

--- src/main/java/org/libertybikes/ai/service/StartupProcedure.java
+++ src/main/java/org/libertybikes/ai/service/StartupProcedure.java
@@ -1,19 +1,7 @@
 package org.libertybikes.ai.service;
 
-import javax.annotation.PostConstruct;
-import javax.inject.Inject;
-import javax.servlet.ServletContextListener;
-import javax.servlet.annotation.WebListener;
-
 /**
  * This class will be initialized when the app first starts. We can use this hook
  * to automatically register our AI service with the core Liberty Bikes services
  */
-@WebListener
-public class StartupProcedure implements ServletContextListener {
-
-    @PostConstruct
-    public void joinFirstRound() {
-        System.out.println("Hello world!");
-    }
-}
+public class StartupProcedure {}
